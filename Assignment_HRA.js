e37a406c725f32dde4b2eed46bc8387cdaa6e9b7
oscarljw <oscarljw@gmail.com>
2020-10-25 19:02:36 +0800

    hi

--- Assignment_HRA.js
+++ Assignment_HRA.js
@@ -6,7 +6,7 @@ var mytext = getUrlVars()["text"];
 	var outputVal2 = outputVal.replace(" ", "~");
 	var outputVal3 = outputVal2.replace(" ", "~");
 	var outputVal4 = outputVal3.replace(" ", "~");
-window.location = 'http://wa.me/?text='+inputVal+' wishing you a Selamat Hari Raya Aidilfitri 2020 %0A%0AOpen this ðŸ‘‡ %0A%0Adcs5604.com/cny2020.html?name='+outputVal4;
+window.location = 'http://wa.me/?text='+inputVal+' wishing you a Selamat Hari Raya Aidilfitri 2020 %0A%0AOpen this ðŸ‘‡ %0A%0Aoscarljw.github.io/Assignment_HRA.html?name='+outputVal4;
 }
 
 function loadname() {
